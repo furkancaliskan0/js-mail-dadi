8ef8df1c837d6cb448f409392c6a31542323b64d
furkanGit <furkancaliskan9801@gmail.com>
2022-11-10 18:05:55 +0100

    Dadi Completed

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -2,3 +2,25 @@
 //sia per il giocatore sia per il computer.
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+
+let myNumber= Math.floor((Math.random)() * 6 )+ 1;
+
+console.log("numero del giocatore ", myNumber)
+
+let numberComputer= Math.floor((Math.random)() * 6 )+ 1;
+
+console.log("numero del computer ", numberComputer)
+
+
+
+if (myNumber > numberComputer){
+    console.log("hai vinto il giocatore ");
+} else{ 
+    console.log("ha perso il giocatore, ha vinto il computer ");
+}
+
+
+
+
+
+
