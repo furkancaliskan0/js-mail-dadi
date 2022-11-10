40c8089d60fef6b554337d7b0898f9c86e1817ad
furkanGit <furkancaliskan9801@gmail.com>
2022-11-10 16:25:57 +0100

    Mail Completed

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,2 +1,22 @@
 // Il Programma deve Chiedere all’utente la sua email, e controllare che sia nella lista di chi può accedere.
 // stampare un messaggio appropriato sull’esito del controllo.
+
+let email = prompt("Inserisci la tua  email");
+
+console.log( email);
+
+
+const myList = ["furkan01@gmail.com", "alessandro98@gmail.com", "luca92@gmail.com"];
+
+for (let index = 0; index <  myList.length; index++) {
+    if (email ===  myList[index]) {
+
+        console.log(`Ciao ${myList[index]} la tua mail c'è nella lista `);
+        
+    }
+    
+}
+
+
+
+
